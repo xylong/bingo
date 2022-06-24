f04cf3c7e32774773c94e4a6fb5994b85a92d6f3
xyl <416319808@qq.com>
2022-06-25 00:00:11 +0800

    gorm

--- db.go
+++ db.go
@@ -1,1 +1,29 @@
 package bingo
+
+import (
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func OpenMysql() *gorm.DB {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DriverName:                "mysql",
+		ServerVersion:             "8.0.13",
+		DSN:                       dsn,
+		Conn:                      nil,
+		SkipInitializeWithVersion: false,
+		DefaultStringSize:         191,
+	}), &gorm.Config{
+		SkipDefaultTransaction:                   false,
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	return db
+}
