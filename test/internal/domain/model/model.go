ec5823841d5fda352f1135f111c9becc016e7fb3
xyl <416319808@qq.com>
2022-07-18 23:51:19 +0800

    保存指定字段

--- test/internal/domain/model/model.go
+++ test/internal/domain/model/model.go
@@ -34,3 +34,8 @@ func (m *Model) GetByID(id int) interface{} {
 	db.DB.First(m.child, id)
 	return m.child
 }
+
+// Save 指定字段保存
+func (m *Model) Save(column ...string) error {
+	return db.DB.Select(column).Save(m.child).Error
+}
