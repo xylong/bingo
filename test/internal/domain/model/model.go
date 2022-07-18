d1d97a9b6a12c245b1383f2d04a7f73aefaf82ff
xyl <416319808@qq.com>
2022-07-19 06:29:35 +0800

    conflict

--- test/internal/domain/model/model.go
+++ test/internal/domain/model/model.go
@@ -3,15 +3,22 @@ package model
 import (
 	"github.com/xylong/bingo/test/internal/lib/db"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 // Model 基础模型
 type Model struct {
-	child interface{}
+	child    interface{}
+	conflict func() clause.OnConflict
 }
 
 func NewModel(data interface{}) *Model {
-	return &Model{child: data}
+	return &Model{
+		child: data,
+		conflict: func() clause.OnConflict {
+			return clause.OnConflict{DoNothing: true}
+		},
+	}
 }
 
 // Compare 字段过滤
@@ -37,5 +44,23 @@ func (m *Model) GetByID(id int) interface{} {
 
 // Save 指定字段保存
 func (m *Model) Save(column ...string) error {
-	return db.DB.Select(column).Save(m.child).Error
+	return db.DB.Clauses(m.conflict()).Select(column).Save(m.child).Error
+}
+
+// Conflict 冲突
+// 唯一约束冲突时的执行方案
+func (m *Model) Conflict(column []string, assignment map[string]interface{}) {
+	columns := make([]clause.Column, 0)
+	for _, item := range column {
+		columns = append(columns, clause.Column{
+			Name: item,
+		})
+	}
+
+	m.conflict = func() clause.OnConflict {
+		return clause.OnConflict{
+			Columns:   columns,
+			DoUpdates: clause.Assignments(assignment),
+		}
+	}
 }
